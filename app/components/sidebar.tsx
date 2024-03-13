9d677532fcf038541df2ac3168442d4dbfdc6c26
zenerq <rokosamje@gmail.com>
2024-03-13 12:09:46 +0100

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Polimat  je organizacija koja vam nudi široko znanje u više različitih predmetnih oblasti.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />

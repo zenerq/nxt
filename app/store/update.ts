63752992a6f7f8ed8502129e61c14abbf46e4f14
zenerq <rokosamje@gmail.com>
2024-03-13 12:20:11 +0100

    Update update.ts

--- app/store/update.ts
+++ app/store/update.ts
@@ -106,7 +106,7 @@ export const useUpdateStore = createPersistStore(
                       if (version === remoteId) {
                         // Show a notification using Tauri
                         window.__TAURI__?.notification.sendNotification({
-                          title: "NextChat",
+                          title: "Polimat",
                           body: `${Locale.Settings.Update.IsLatest}`,
                           icon: `${ChatGptIcon.src}`,
                           sound: "Default",

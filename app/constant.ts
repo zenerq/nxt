d30c0796fd47980a0d65bc8b1fc5869b31599b23
zenerq <rokosamje@gmail.com>
2024-03-13 19:44:27 +0100

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -128,6 +128,15 @@ export const DEFAULT_MODELS = [
     },
   },
   {
+    name: "gpt-4-1106-preview",
+    available: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  },
+  {
     name: "gpt-3.5-turbo-16k",
     available: true,
     provider: {

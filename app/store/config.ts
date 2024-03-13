a0e0d452abe9df36d9b3a3f08146e282bb090983
zenerq <rokosamje@gmail.com>
2024-03-13 21:25:08 +0100

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -39,7 +39,7 @@ export const DEFAULT_CONFIG = {
 
   disablePromptHint: false,
 
-  dontShowMaskSplashScreen: false, // dont show splash screen when create chat
+  dontShowMaskSplashScreen: true, // dont show splash screen when create chat
   hideBuiltinMasks: true, // dont add builtin masks
 
   customModels: "",

6fe23c00e7ad59cb0dcebaa11b4069363dc92442
zenerq <rokosamje@gmail.com>
2024-03-13 12:19:38 +0100

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Polimat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Vaš lični AI pomoćnik
+          Vaš lični AI asistent
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
